5947898056707ac9d08e4b5eb9a025b7743dfda0
Артур <kpro83665@gmail.com>
2025-06-18 09:53:20 +0300

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -2,33 +2,28 @@ public class Account {
     private int id;
     private String ownerName;
     private double balance;
-
     public Account(int id, String ownerName, double balance) {
         this.id = id;
         this.ownerName = ownerName;
         this.balance = balance;
     }
-
     public int getId() {
         return id;
     }
-
     public void deposit(double amount) {
         balance += amount;
-        System.out.println("На рахунок ID " + id + " зараховано " + amount + " грн.");
+        System.out.println("На счет ID " + id + " засчитано " + amount + " грн.");
     }
-
     public void withdraw(double amount) {
         if (balance >= amount) {
             balance -= amount;
-            System.out.println("З рахунку ID " + id + " знято " + amount + " грн.");
+            System.out.println("Со счета ID " + id + " снято " + amount + " грн.");
         } else {
-            System.out.println("Недостатньо коштів на рахунку ID " + id);
+            System.out.println("Недостаточно средств на счете ID " + id);
         }
     }
-
     @Override
     public String toString() {
-        return "Рахунок ID: " + id + ", Власник: " + ownerName + ", Баланс: " + balance + " грн.";
+        return "Счет ID: " + id + ", Владелец: " + ownerName + ", Баланс: " + balance + " грн.";
     }
 }
